76500f590c73c9fa5a09b738cc819bea8bb7d617
Saif Khayoon <88499318+SaifKhayoon@users.noreply.github.com>
2023-03-01 20:25:39 -0500

    reconfiguring the upsampler

--- src/upsampler.jl
+++ src/upsampler.jl
@@ -1,14 +1,24 @@
-module Upsample
-
-using Flux
-
-# Function to perform upsampling on an input tensor
-function upsample(input_tensor, scale_factor)
+function upsample(input_tensor, scale_factor, dims=2)
     # Define the upsampling layer
-    upsample_layer = Flux.upsample(input_tensor, scale_factor)
+    if dims == 2
+        upsample_layer = Flux.upsample(input_tensor, scale_factor)
+    else
+        upsample_layer = Flux.upsample(input_tensor, (scale_factor...,))
+    end
+
+    # Add a Tanh activation function
+    upsample_layer = tanh.(upsample_layer)
+
+    # Add a DDim layer for dynamic upsampling
+    ddim_layer = Flux.DDim(size(upsample_layer))
+    upsampled_tensor = ddim_layer(upsample_layer, (size(input_tensor) ÷ scale_factor...,))
+
+    # Add instance normalization
+    upsampled_tensor = Flux.instance_norm(upsampled_tensor)
+
+    # Add dropout regularization
+    upsampled_tensor = Flux.dropout(upsampled_tensor, 0.2)
 
     # Return the upsampled tensor
-    return upsample_layer
+    return upsampled_tensor
 end
-
-end # module
